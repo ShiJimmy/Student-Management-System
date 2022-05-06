639afbf851b492262d505c052c6f6674476e5d28
ShiJimmy <105063873+ShiJimmy@users.noreply.github.com>
2022-05-06 22:40:04 +0800

    Add files via upload

---
+++ client.cpp
@@ -0,0 +1,53 @@
+#include <iostream>
+#include <cstdio>
+
+int main() {
+    printf("Client\n");
+
+    while (true)
+    {
+        int inputins;
+        printf("\n|********* Welcome to use info system ***********|");
+        printf("\n|------------1:  Inquiry my timetable -----------|");
+        printf("\n|------------2:  Add one user -------------------|");
+        printf("\n|------------3:  Update one user ----------------|");
+        printf("\n|------------4:  Delete one user ----------------|");
+        printf("\n|------------5:  Edit one course ----------------|");
+        printf("\n|------------0:  Exit ---------------------------|");
+        printf("\n|************************************************|\n");
+        puts("Please input an instruction:");
+        scanf("%d", &inputins);
+        if (inputins == 0)
+        {
+            puts("Good bye!\n");
+            break;
+        }
+        else if (inputins == 1)
+        {
+            puts("1\n");
+        }
+        else if (inputins == 2)
+        {
+            puts("2\n");
+        }
+        else if (inputins == 3)
+        {
+            puts("3\n");
+        }
+        else if (inputins == 4)
+        {
+            puts("4\n");
+        }
+        else if (inputins == 5)
+        {
+            puts("5\n");
+        }
+        else
+        {
+            puts("Input invalid! Please try again.\n");
+        }
+    }
+
+
+    return 0;
+}
